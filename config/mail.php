7f57efff4244129df28547c133b0c9e60842f63a
Tim Joosten <Tjoosten3@gmail.com>
2017-06-23 20:08:00 +0200

    Fix email configuration

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@activisme.be'),
+        'name' => env('MAIL_FROM_NAME', 'Activisme_BE'),
     ],
 
     /*
