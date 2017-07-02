1a6ae08dc0ec5fd3c8b2fe02d084d78ac53706ef
Tim Joosten <Tjoosten3@gmail.com>
2017-07-02 22:52:42 +0200

    Implement validation errors on petition create view

--- resources/views/petitions/create.blade.php
+++ resources/views/petitions/create.blade.php
@@ -11,50 +11,67 @@
                     <form action="{{ route('petitions.store') }}" method="POST" class="form-horizontal" enctype="multipart/form-data">
                         {{ csrf_field() }}
 
-                        <div class="form-group">
+                        <div class="form-group {{ $errors->has('title') || $errors->has('total_signatures') ? 'has-error' : '' }}">
                             <label class="control-label col-md-2">
                                 Petitie titel: <span class="text-danger">*</span>
                             </label>
 
                             <div class="col-md-7">
-                                <input type="text" class="form-control" placeholder="Petitie titel" name="title">
+                                <input type="text" class="form-control" {{ old('title') }} placeholder="Petitie titel" name="title">
                             </div>
 
                             <div class="col-md-3">
-                                <input type="text" class="form-control" placeholder="Aantal handtekeningen" name="total_signatures">
+                                <input type="text" class="form-control" value="{{ old('total_signatures') }}" placeholder="Aantal handtekeningen" name="total_signatures">
+                            </div>
+
+                            <div class="col-md-offset-2 col-md-10">
+                                @if ($errors->has('title') || $errors->has('total_signatures'))
+                                    @if ($errors->has('title')) <small class="help-block"> {{ ucfirst($errors->first('title')) }} </small> @endif
+                                    @if ($errors->has('total_signatures')) <small class="help-block"> {{ ucfirst($errors->first('total_signatures')) }} </small> @endif
+                                @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
                             <label class="control-label col-md-2">
                                 Afbeelding: <span class="text-danger">*</span>
                             </label>
 
                             <div class="col-md-10">
                                 <input type="file" name="image" class="form-control">
+                                @if ($errors->has('image')) <small class="help-block">{{ ucfirst($errors->first('image')) }}</small> @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group {{ $errors->has('categories') ? ' has-error' : '' }}">
                             <label class="control-label col-md-2">
                                 Categorieen: <span class="text-danger">*</span>
                             </label>
 
                             <div class="col-md-10">
-                                <input type="text" name="categories" class="form-control" placeholder="Petitie categorieen">
-                                <small class="help-block">* Categorieen kunnen gescheiden worden door een comma. bv: welzijn, politiek</small>
+                                <input type="text" name="categories" value="{{ old('categories') }}" class="form-control" placeholder="Petitie categorieen">
+
+                                @if ($errors->has('categories'))
+                                    <small class="help-block"> {{ ucfirst($errors->first('categories')) }} </small>
+                                @else
+                                    <small class="help-block">* Categorieen kunnen gescheiden worden door een comma. bv: welzijn, politiek</small>
+                                @endif
                             </div>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group {{ $errors->has('text') ? 'has-error' : '' }}">
                             <label class="control-label col-md-2">
                                 Petitie tekst: <span class="text-danger">*</span>
                             </label>
 
                             <div class="col-md-10">
                                 <textarea name="text" id="summernote">
-                                    <p><b>Zet hier je petitie tekst</b></p>
+                                    @if (! is_null(old('text'))) {{ old('text') }}
+                                    @else <p><b>Zet hier je petitie tekst</b></p>
+                                    @endif
                                 </textarea>
+
+                                @if ($errors->has('text')) <small class="help-block"> {{ ucfirst($errors->first('text')) }} </small> @endif
                             </div>
                         </div>
 
