2a1c89d04cc1d54c070d3258eda607e96fc17f10
Tim Joosten <Tjoosten3@gmail.com>
2017-07-04 06:29:32 +0200

    Implement missing use statement

--- database/seeds/CountryTableSeeder.php
+++ database/seeds/CountryTableSeeder.php
@@ -2,6 +2,7 @@
 
 use ActivismeBE\Countries;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CountryTableSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class CountryTableSeeder extends Seeder
     public function run()
     {
         DB::table('countries')->delete();
+
         Countries::insert([
             [
                 'long_name' => 'Afghanistan',
