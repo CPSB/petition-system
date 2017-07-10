4f66cfa24fdf0758dd5e5fb39d70f95921232adc
Tjoosten <Tjoosten3@gmail.com>
2017-07-10 03:40:24 +0200

    Update on testing

---
+++ tests/Feature/AccountSettingsTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AccountSettingsTest extends TestCase
+{
+    public function testNotAuthencated()
+    {
+
+    }
+
+    public function testAuthencated()
+    {
+
+    }
+
+    public function testChangeSettingsValidationErrors()
+    {
+
+    }
+
+    public function testChangeSettingsNoAuth()
+    {
+
+    }
+
+    public function testChangeSettingsOk()
+    {
+
+    }
+
+    public function testChangePasswordNoAuth()
+    {
+
+    }
+
+    public function testChangePasswordValidationErrors()
+    {
+
+    }
+
+    public function testChangePasswordValidationOk()
+    {
+
+    }
+}
